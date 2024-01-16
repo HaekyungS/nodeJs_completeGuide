47ab942dd290c27c20ae63851f9ed3a63a825e11
HaekyungSeong <haekyungs95@gmail.com>
2024-01-16 15:59:15 +0900

    주석 해설 추가

--- module_2/play.js
+++ module_2/play.js
@@ -1,27 +1,25 @@
-
 const fetchData = () => {
-    const promise = new Promise((resolve, reject)=> {
-        setTimeout( ()=> {
-            resolve('Done');
-        },1500);
-    });
-    return promise;
-
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("promise in setTimeOut Done");
+    }, 1500);
+  });
+  //   리턴값이 프로미스니까 프로미스가 반환.
+  return promise;
 };
 
-setTimeout(()=> {
-    console.log('timer is done');
-    fetchData()
-    .then(text=>{
-        console.log(text);
-        return fetchData();
+setTimeout(() => {
+  console.log("setTimerout timer is done");
+  //   fetchData 함수 실행 -> 함수 내 프로미스 내 setTimeout
+  fetchData()
+    .then((text) => {
+      console.log(text);
+      return fetchData();
     })
-    .then(text2=> {
-        console.log(text2);
+    .then((text2) => {
+      console.log(text2);
     });
+}, 2000);
 
-},2000);
-
-
-console.log('hello');
-console.log('hi');
+console.log("hello");
+console.log("hi");
