7b5d14637b30535ac4b27d0c234b301e500fc0de
HaekyungSeong <haekyungs95@gmail.com>
2024-01-16 15:27:58 +0900

    주석추가

--- module_1/first-app.js
+++ module_1/first-app.js
@@ -1,3 +1,6 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.writeFileSync('hello.txt', 'hello from node js');
+// fs => file system의 줄임말.
+// writeFileSync(파일명, 파일 내 들어갈 텍스트)
+// 파일명 부분에 경로를 포함해서 쓰면 해당 경로에 생김.
+fs.writeFileSync("../hello.txt", "생겨봐");
