58599177eae9412e1010829eb5e3f031ea106070
HaekyungSeong <haekyungs95@gmail.com>
2024-01-17 15:20:41 +0900

    주석 추가

--- module_3/app.js
+++ module_3/app.js
@@ -1,13 +1,13 @@
-// 노드 js 서버 만들기 
-const http = require('http'); // http 객체를 가져온다. 
+// 노드 js 서버 만들기
+const http = require("http"); // http 객체를 가져온다.
 
-const routes = require('./routes.js');
+const routes = require("./routes.js");
 
 console.log(routes.someText);
 
 const server = http.createServer(routes.handler); //들어오는 모든 request에 대하여 해당 펑션을 실행하도록  //create Server Method가 서버를 반환함 . 따라서 변수에 넣어놓는다.
 
-server.listen(3000);  
+server.listen(3000);
 // start script
-//만든 서버 객체의 listen 메서드를 이용해 원하는 포트, 도메인(여기서는 localhost)에 서버를 연결한다. 
-//이를 통해 request를 계속 받게된다. 
+//만든 서버 객체의 listen 메서드를 이용해 원하는 포트, 도메인(여기서는 localhost)에 서버를 연결한다.
+//이를 통해 request를 계속 받게된다.
